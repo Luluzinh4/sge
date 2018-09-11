e307e35e205272a72a7db77ef4b87feb24536230
Luana <42622079+Luluzinh4@users.noreply.github.com>
2018-09-10 23:02:34 -0300

    Update Curso.java

--- src/main/java/br/edu/ifsp/spo/lg2a2/sge/entidades/Curso.java
+++ src/main/java/br/edu/ifsp/spo/lg2a2/sge/entidades/Curso.java
@@ -3,10 +3,16 @@ package br.edu.ifsp.spo.lg2a2.sge.entidades;
 import java.util.Collection;
 
 public class Curso {
-	
+
 	private Collection<Turma> turmas;
+	private String codigoCurso;
+	
 	public Collection<Turma> getTurmas() {
 		return this.turmas;
+	}
+	
+	public String getCodigoCurso() {
+		return this.codigoCurso;
 	}
 
 }
